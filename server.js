ee3e35de63b975cfedf038a17bd33e7472166c1c
Artur C <araghast94@icloud.com>
2023-10-02 20:38:44 +0200

    Prepare production build

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 8000;
 const cors = require('cors');
+const path = require('path');
 
 // import routes
 const testimonialsRoutes = require('./routes/testimonials.routes');
@@ -17,10 +17,20 @@ app.use('/api', seatsRoutes); // add seats routes
 app.use('/api', concertsRoutes); // add concerts routes
 
 
+
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 })
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
+});
